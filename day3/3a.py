14fd4fe07980cac0a7db5bd97b26a00a85e202ab
David Harris <daharr@umich.edu>
2020-12-04 23:55:05 -0500

    3a codegolf

--- day3/3a.py
+++ day3/3a.py
@@ -1,21 +1,3 @@
 #!/usr/bin/env python3
-with open('input.txt', 'r') as f:
-    input = f.read().splitlines()
 
-rows = len(input)
-columns=len(input[0])
-
-currentRow=0
-currentColumn=0
-count = 0
-while currentRow < rows:
-    currentRow+=1
-    currentColumn+=3
-    if currentRow >= rows:
-        break
-    if input[currentRow][currentColumn % columns] == '#':
-        count +=1
-
-print(count)
-
-[(1,1),(3,1),(5,1),(7,1),(1,2)]
+print(sum([row[lineNumber * 3 % len(row)] == '#' for lineNumber, row in enumerate(open('input.txt').read().splitlines())]))
