f7c4bbe03163439dbd4bba66b5ac0176813d535d
David Harris <daharr@umich.edu>
2020-12-05 11:24:25 -0500

    5b codegolf

--- day5/5b.py
+++ day5/5b.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
-import more_itertools
+from itertools import groupby
+from operator import itemgetter
 
-print([list(group) for group in more_itertools.consecutive_groups(set(range(1024)) - set([int(line.replace("F", "0").replace("B", "1").replace("L", "0").replace("R", "1"), 2) for line in open('input.txt', 'r').read().splitlines()]))][1][0])
+print([list(map(itemgetter(1), g)) for k,g in groupby(enumerate(set(range(1024)) - set([int(line.replace("F", "0").replace("B", "1").replace("L", "0").replace("R", "1"), 2) for line in open('input.txt', 'r').read().splitlines()])),  key=lambda x: x[0] - x[1])][1][0])
