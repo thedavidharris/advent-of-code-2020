f00e7405247d24a0fe6fa739d23d81773aec90f5
David Harris <daharr@umich.edu>
2020-12-05 17:06:11 -0500

    codegolf 3b

--- day3/3b.py
+++ day3/3b.py
@@ -1,24 +1,4 @@
 #!/usr/bin/env python3
-with open('input.txt', 'r') as f:
-    input = f.read().splitlines()
+from math import prod
 
-rows = len(input)
-columns=len(input[0])
-
-slopes = [(1,1),(3,1),(5,1),(7,1),(1,2)]
-
-multipliedTrees = 1
-for slope in slopes:
-    currentRow=0
-    currentColumn=0
-    count = 0
-    while currentRow < rows:
-        currentColumn+=slope[0]
-        currentRow+=slope[1]
-        if currentRow >= rows:
-            break
-        if input[currentRow][currentColumn % columns] == '#':
-            count +=1
-    multipliedTrees *= count
-
-print(multipliedTrees)
+print((lambda m:prod(sum(line[i*dx %len(line)] == '#' for i,line in enumerate(m[::dy])) for dx,dy in [(3,1),(1,1),(5,1),(7,1),(1,2)]))(open('input.txt').read().splitlines()))
