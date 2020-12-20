175c19c99a1ea60be29b7bdb94a18fb478408ac3
David Harris <daharr@umich.edu>
2020-12-19 20:02:16 -0500

    day19

---
+++ day19/19a.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import re
+
+
+MAX_DEPTH = 14
+rules = {}
+
+def build_regex(rule_num, depth):
+    if depth > MAX_DEPTH:
+        return "" 
+
+    if rules[rule_num].startswith('"'):
+        return rules[rule_num].replace('"', '')
+
+    current_rules = []
+
+    for rule_part in rules[rule_num].split("|"):
+        inner = []
+        for reference in rule_part.split():
+            inner.append(build_regex(reference, depth + 1))
+        current_rules.append("".join(inner))
+
+    return "(" + "|".join(current_rules) + ")"
+
+ 
+with open("input.txt") as f:
+    rules_input, messages = f.read().split("\n\n")
+
+for rule_input in rules_input.splitlines():
+    num, definition = rule_input.split(": ")
+    rules[num] = definition
+
+regex = re.compile(build_regex("0", 0))
+
+messages = messages.splitlines()
+print(sum([regex.fullmatch(message) is not None for message in messages]))
+
